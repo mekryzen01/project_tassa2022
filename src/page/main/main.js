676a47c90c64847618801cff5abf9f7645c5f052
Methiphat <631463026@crru.ac.th>
2023-02-14 09:37:12 +0700

    update_final
    end project

--- src/page/main/main.js
+++ src/page/main/main.js
@@ -37,6 +37,7 @@ export default function Main() {
     const [barmonk, setBarmonk] = useState([]);
     const [res, setRes] = useState([]);
     const [meta, setMeta] = useState([]);
+    const [royal, setRoyal] = useState([]);
     const [chartData, setChartData] = useState({
         datasets: [],
     });
@@ -58,6 +59,7 @@ export default function Main() {
     async function getstart() {
         await getActivities();
         await getvideo();
+        await get03();
         await getmeta();
         await getdataChatAsset();
         await getchart();
@@ -71,6 +73,11 @@ export default function Main() {
             setdataActivities(response.data)
         })
     }
+
+    async function get03() {
+        await axios.get("https://isethailand.org/tassa/server/chart03.php").then((response) => setRoyal(response.data))
+    }
+
 
     async function getmeta() {
         await axios.get("https://isethailand.org/tassa/server/getmeta.php").then((response) => setMeta(response.data))
@@ -124,7 +131,7 @@ export default function Main() {
     };
     const getdataChatroyal = async () => {
         await axios
-            .get("https://isethailand.org/tassa/server/chart03.php")
+            .get("https://isethailand.org/tassa/server/chart04.php")
             .then((result) => {
                 userdata02 = result.data;
                 setBarmonk(userdata02);
@@ -138,31 +145,20 @@ export default function Main() {
             labels: userdata02.map((item) => item.type_name),
             datasets: [
                 {
-                    label: userdata02.map((item) => item.type_name),
+                    label: '',
                     data: userdata02.map((item) => item.countall),
                     borderColor: [
                         'rgba(255, 99, 132)',
                         'rgba(54, 162, 235)',
                         'rgba(255, 206, 86)',
-                        'rgba(255, 206, 86)',
-                        'rgba(255, 206, 86)',
-                        'rgba(255, 206, 86)',
-                        'rgba(255, 206, 86)',
-                        'rgba(255, 206, 86)',
-                        'rgba(255, 206, 86)',
-                        'rgba(255, 206, 86)',
+                        'rgba(155, 206, 86)',
+
                     ],
                     backgroundColor: [
                         'rgba(255, 99, 132)',
-                        'rgba(255, 99, 132)',
-                        'rgba(255, 99, 132)',
-                        'rgba(255, 99, 132)',
-                        'rgba(54, 162, 235)',
-                        'rgba(54, 162, 235)',
                         'rgba(54, 162, 235)',
                         'rgba(255, 206, 86)',
-                        'rgba(255, 206, 86)',
-                        'rgba(255, 206, 86)',
+                        'rgba(155, 206, 86)',
                     ],
                 },
             ],
@@ -175,7 +171,7 @@ export default function Main() {
                 },
                 title: {
                     display: true,
-                    text: "โครงการภายใต้สถาบันเศรษฐกิจพอเพียง" + " " + "จำนวน" + " " + "71" + " " + "โครงการ",
+                    text: "โครงการภายใต้สถาบันเศรษฐกิจพอเพียง" + " " + "จำนวน" + " " + "26" + " " + "โครงการ",
                 },
             },
         });
@@ -194,12 +190,12 @@ export default function Main() {
     return (
         <>
             <div className="container">
-                <div className="row my-3 mb-5 pb-5">
-                    <div className="col-12 col-md-6 col-lg-5">
+                <div className="row my-3 mb-2 pb-5">
+                    <div className="col-12 col-md-6 col-lg-6">
                         {datavideo.map((i, k) => (
                             <div className="row"><iframe
-                                width="70%"
-                                height="300px"
+                                width="100%"
+                                height="330px"
                                 src={i.video_url}
                                 title="YouTube video player"
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -211,12 +207,12 @@ export default function Main() {
                     <div className="col-12 col-md-6 col-lg-6">
                         <div className="row">
                             {dataActivities.map((item, index) => (
-                                <div className="col-12 col-md-6 col-lg-4 text-center my-3" key={index}>
+                                <div className="col-12 col-md-6 col-lg-6 text-center my-3" key={index}>
                                     <Link to={`/tassa2022/TypeActivities/${item.typeact_id}`}>
-                                        <div className="card shadow bg-body" id="hover1" style={{ width: "100%", height: "250px" }}>
+                                        <div className="card shadow bg-body" id="hover1" style={{ width: "100%" }}>
                                             <div className="card-body">
                                                 <div className="row justify-content-center">
-                                                    <img src={item.imagetype} alt="" width="100%"/>
+                                                    <img src={item.imagetype} alt="" width="100%" />
                                                 </div>
                                                 <div className="row justify-content-center">
                                                     <p>{item.typeact_name}</p>
@@ -226,57 +222,66 @@ export default function Main() {
                                     </Link>
                                 </div>
                             ))}
-                            <div className="col-12 col-md-6 col-lg-4 text-center my-3">
-                                <div className="card" style={{ width: "100%", height: "250px" }}>
+                            <div className="col-12 col-md-6 col-lg-6 text-center my-3">
+                                <div className="card" style={{ width: "100%" }}>
                                     <div className="card-body">
                                         <Doughnut options={chartOptions} data={chartData} />
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-12 col-md-4 col-lg-6 mt-2">
-                                <div className="card" style={{ width: "100%" }}>
+                            <div className="col-12 col-md-4 col-lg-6 mt-3">
+                                <div className="card" style={{ width: "100%", height: "160px,auto" }}>
                                     <div className="card-body">
                                         <div className="text-center">
                                             <p>นักวิจัย</p>
                                             {userdata03.map((i, k) => (
-                                                <h1>{i.countall}</h1>
+                                                <h1><b>{i.countall}</b></h1>
                                             ))}
                                         </div>
                                     </div>
                                 </div>
-                                <div className="card mt-3" style={{ width: "100%" }}>
+                                <br />
+                                <br />
+                                <div className="card" style={{ width: "100%", height: "160px,auto" }}>
                                     <div className="card-body">
                                         <div className="text-center">
                                             <p>โครงการพระราชดำริ</p>
-                                            <h1>26</h1>
+                                            {royal.map((i) => (
+                                                <h1><b>{i.countall}</b></h1>
+                                            ))}
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-12 col-md-6 col-lg-6 text-center mt-2">
-                                <div className="card" style={{ width: "100%",height:"270px" }}>
+                            <div className="col-12 col-md-6 col-lg-12  mt-2">
+                                <div className="card" style={{ width: "100%" }}>
                                     <div className="card-body">
-                                        <b>สื่อการเรียนรู้เสมือนจริง</b>
+                                        <div className="text-center">
+                                            <b id="textheader" >สื่อการเรียนรู้เสมือนจริง</b>
+                                        </div>
                                         {meta.map((i, k) => (
-                                            <div className="row justify-content-center">
+                                            <div className="row">
                                                 <div className="col-12 col-md-6 col-lg-12 mt-2" key={k}>
-                                                    <p><a href={i.video} target="_blank" id="black">
-                                                       <h5>{i.meta_name}</h5> 
-                                                    </a></p>
-                                                    
+                                                    <button className="btn btn-">
+                                                         <a href={i.video} target="_blank" id="black">
+                                                        <h5>{i.meta_name}</h5>
+                                                    </a>
+                                                    </button>
+                                                   
                                                 </div>
-
                                             </div>
                                         ))}
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-12 col-md-4 col-lg-12 mt-5">
-                                <div className="card" style={{ width: "100%" }}>
-                                    <div className="card-body">
-                                        <Bar options={chartOptions01} data={chartData01} />
-                                    </div>
-                                </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-4 col-lg-12 mb-5">
+                        <div className="card" style={{ width: "100%" }}>
+                            <div className="card-body">
+                                <Bar options={chartOptions01} data={chartData01} />
                             </div>
                         </div>
                     </div>
